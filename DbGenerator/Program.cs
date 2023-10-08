d8ce25cc6e335fbe93d5df122054e99561cacae0
elianiva <dicha.arkana03@gmail.com>
2023-10-08 09:35:03 +0700

    refactor: type imports inconsistencies

--- DbGenerator/Program.cs
+++ DbGenerator/Program.cs
@@ -1,28 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using DbGenerator;
 using DbGenerator.Business.FuriganaDomain;
 using DbGenerator.Business.JmdictDomain;
-using SQLitePCL;
 
 string dataPath = Path.Join(Directory.GetCurrentDirectory(), "Data");
 
 // use a custom sqlite with icu support
-SQLite3Provider_dynamic_cdecl.Setup("sqlite3", new NativeLibraryAdapter(Path.Join(dataPath, "sqlite/.libs/libsqlite3.so")));
-SQLitePCL.raw.SetProvider(new SQLite3Provider_dynamic_cdecl());
+SQLitePCL.SQLite3Provider_dynamic_cdecl.Setup("sqlite3", new NativeLibraryAdapter(Path.Join(dataPath, "sqlite/.libs/libsqlite3.so")));
+SQLitePCL.raw.SetProvider(new SQLitePCL.SQLite3Provider_dynamic_cdecl());
 
-FuriganaSource furiganaSource = new(Path.Join(dataPath, "JmdictFurigana.json"));
+FuriganaSource furiganaSource = new FuriganaSource(Path.Join(dataPath, "JmdictFurigana.json"));
 JmdictSource jmdictSource = new(Path.Join(dataPath, "JMdict_e.gz"));
 SQLiteInserter inserter = new(Path.Join(dataPath, "jmdict.db"));
 
 try
 {
     Console.WriteLine("====[ Getting furigana entries ]====");
-    System.Collections.Generic.IEnumerable<FuriganaEntry> furiganaEntries = await furiganaSource.GetEntries();
+    IEnumerable<FuriganaEntry> furiganaEntries = await furiganaSource.GetEntries();
 
     Console.WriteLine("====[ Getting jmdict entries ]====");
-    System.Collections.Generic.IEnumerable<JmdictEntry> jmdictEntries = await jmdictSource.GetEntries();
+    IEnumerable<JmdictEntry> jmdictEntries = await jmdictSource.GetEntries();
 
     Console.WriteLine("====[ Inserting entries ]====");
     Stopwatch stopwatch = new();
@@ -34,5 +34,5 @@ try
 catch (Exception ex)
 {
     Console.WriteLine(ex);
-    System.Environment.Exit(1);
+    Environment.Exit(1);
 }
